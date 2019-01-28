d0149f839c33ff7303499eb48a94aa6358e202b2
With_Mez <myname_jay_work1010@hotmail.com>
2019-01-28 11:58:31 +0700

    Add files via upload

---
+++ test.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float f;
+    printf("Please enter a float number: ");
+    scanf("%f", &f);
+    printf("%f \n", f);
+    printf("f = %.2f \n", f);
+    return 0;
+}
