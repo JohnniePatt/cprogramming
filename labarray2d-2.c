af3a6e4670b71f155fa05c60d3b15a964c00dfec
With_Mez <myname_jay_work1010@hotmail.com>
2019-02-25 13:02:59 +0700

    Add files via upload

---
+++ labarray2d-2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#define MAX 10 
+int main()
+{
+    int MatA[MAX][MAX]={}, MatB[MAX][MAX]={}, MatC[MAX][MAX]={}; 
+    int x,y,ra,rb,ca,cb;
+    printf("Matrix A\nNumber of rows = ")
+    scanf("%d",&ra);
+    printf("Number of columns = ")
+    scanf("%d",&ca);
+    for(x = 0,y = 0; x)
+    
+
+}
